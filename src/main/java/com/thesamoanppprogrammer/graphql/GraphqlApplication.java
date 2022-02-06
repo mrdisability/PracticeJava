51f37d5680c63483bbc859fc23f1e78fdb50d282
King Ioane <thesamoanppprogrammer@gmail.com>
2022-02-07 01:42:14 +1300

    Added cors to read from react frontend

--- src/main/java/com/thesamoanppprogrammer/graphql/GraphqlApplication.java
+++ src/main/java/com/thesamoanppprogrammer/graphql/GraphqlApplication.java
@@ -3,12 +3,28 @@ package com.thesamoanppprogrammer.graphql;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class GraphqlApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GraphqlApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurerAdapter() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+//				registry.addMapping("/graphql").allowedOrigins("http://localhost:3000");
+
+				registry.addMapping("/**").allowedOrigins("http://localhost:3000");
+			}
+		};
 	}
 
 }
